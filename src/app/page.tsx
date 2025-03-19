6f38ccef2efe940ceba1746e6059defe51b7c83b
tsuki917 <einstrom917@gmail.com>
2025-03-19 16:15:01 +0900

    feat:各都道府県名データのフェッチ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import { fetcher } from "./lib/fetcher";
 import SelectPref from "./ui/checkbox/SelectPref";
 import Header from "./ui/common/Header";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetcher("/api/v1/prefectures", {
+    headers: {
+      "X-API-KEY": process.env.NEXT_PUBLIC_API_KEY ?? "",
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    next: {
+      revalidate: 3600,
+    },
+  });
   return (
     <main>
       <Header />
-      <SelectPref />
+      <SelectPref prefs={res.result} />
     </main>
   );
 }
